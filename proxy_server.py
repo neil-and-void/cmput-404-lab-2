d4aa2c9265f7fba9a8b1ab84b373041512633d41
Neilzon <neilcviloria@gmail.com>
2023-01-18 23:11:49 -0700

    stuff

---
+++ proxy_server.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import socket
+import time
+
+#define address & buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    
+        #QUESTION 3
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+        #bind socket to address
+        s.bind((HOST, PORT))
+        #set to listening mode
+        s.listen(2)
+        
+        try:
+            #continuously listen for connections
+            while True:
+                conn, addr = s.accept()
+                print("Connected by", addr)
+                
+                #recieve data, wait a bit, then send it back
+                # get the host name from the 
+                full_data = conn.recv(BUFFER_SIZE)
+
+                # perform outgoing query
+                host = "www.google.com"
+                payload = f'GET / HTTP/1.0\r\nHost: {host}\r\n\r\n'
+                buffer_size = 4096
+
+                http_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+                remote_ip = socket.gethostbyname(host)
+
+                http_socket.connect((remote_ip, 80));
+                
+                http_socket.send(payload.encode())
+                # perform outgoing request end
+
+                print("LKSDFJKLDS")
+                #continue accepting data until no more left
+                response_data = b""
+                while True:
+                    data = http_socket.recv(buffer_size)
+                    if not data:
+                        break
+                    response_data += data
+                
+                time.sleep(0.5)
+                conn.sendall(response_data)
+                
+                conn.close()
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    main()
